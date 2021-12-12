92443450c3b97e4b377cfeb9911c7b2820cc0bed
ismakova <ismakova.dayana@mail.ru>
2021-12-12 14:01:10 +0500

    add filtering

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -17,6 +17,11 @@ const Main = () => {
         {id: 2, topic: "degradation", type: 'comment'},
         {id: 3, topic: "life of mouse", type: 'comment'},
         {id: 4, topic: "life of mouse", type: 'picture'},
+        {id: 4, topic: "life of mouse", type: 'picture'},
+        {id: 4, topic: "life of mouse", type: 'comment'},
+        {id: 4, topic: "life of mouse", type: 'post'},
+        {id: 4, topic: "life of mouse", type: 'picture'},
+        {id: 4, topic: "miley syrus is my love", type: 'picture'},
     ]
     const topics = [
         {topic: 'degradation', value: 1},
@@ -28,6 +33,7 @@ const Main = () => {
 
     const topicChange = (event) => {
         setTopic(event.target.value);
+
     };
     const typeChange = (event) => {
         setType(event.target.value);
@@ -36,57 +42,59 @@ const Main = () => {
     return (
         <div>
             <Box my={2}>
-            <Grid container justifyContent={"start"} spacing={4}>
-               <Grid item> </Grid>
-                <Grid item xs={2}>
-                    <FormControl fullWidth>
-                        <InputLabel id="topic">Тема</InputLabel>
-                        <Select
-                            labelId="topic"
-                            id="topic"
-                            value={topic}
-                            label="Topic"
-                            onChange={topicChange}
-                        >
-                            <MenuItem value={0}>
-                                <em>Все</em>
-                            </MenuItem>
-                            {topics.map((item) => <MenuItem value={item.value}>{item.topic}</MenuItem>)}
+                <Grid container justifyContent={"start"} spacing={4}>
+                    <Grid item> </Grid>
+                    <Grid item xs={2}>
+                        <FormControl fullWidth>
+                            <InputLabel id="topic">Тема</InputLabel>
+                            <Select
+                                labelId="topic"
+                                id="topic"
+                                value={topic}
+                                label="Topic"
+                                onChange={topicChange}
+                            >
+                                <MenuItem value={0}>
+                                    <em>Все</em>
+                                </MenuItem>
+                                {topics.map((item) => <MenuItem value={item.topic}>{item.topic}</MenuItem>)}
 
-                        </Select>
-                        {/*<FormHelperText>With label + helper text</FormHelperText>*/}
-                    </FormControl>
+                            </Select>
+                            {/*<FormHelperText>With label + helper text</FormHelperText>*/}
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <FormControl fullWidth>
+                            <InputLabel id="type">Тип</InputLabel>
+                            <Select
+                                labelId="type"
+                                id="type"
+                                value={type}
+                                label="Type"
+                                onChange={typeChange}
+                            >
+                                <MenuItem value={0}>
+                                    <em>Все</em>
+                                </MenuItem>
+                                <MenuItem value={"comment"}>Комментарии</MenuItem>
+                                <MenuItem value={"post"}>Посты</MenuItem>
+                                <MenuItem value={"picture"}>Картинки</MenuItem>
+
+                            </Select>
+                            {/*<FormHelperText>With label + helper text</FormHelperText>*/}
+                        </FormControl>
+                    </Grid>
                 </Grid>
-
-                <Grid item xs={2}>
-                    <FormControl fullWidth>
-                        <InputLabel id="type">Тип</InputLabel>
-                        <Select
-                            labelId="type"
-                            id="type"
-                            value={type}
-                            label="Type"
-                            onChange={typeChange}
-                        >
-                            <MenuItem value={0}>
-                                <em>Все</em>
-                            </MenuItem>
-                            <MenuItem value={0}>Комментарии</MenuItem>
-                            <MenuItem value={0}>Посты</MenuItem>
-                            <MenuItem value={0}>Картинки</MenuItem>
-
-                        </Select>
-                        {/*<FormHelperText>With label + helper text</FormHelperText>*/}
-                    </FormControl>
-                </Grid>
-            </Grid>
             </Box>
             {/*<Divider />*/}
             <Grid container justifyContent={"center"} spacing={2}>
                 <Stack>
-                    {sample.map((item) =>
-                        <CustomCard id={item.id} topic={item.topic} type={item.type}/>
-                    )}
+                    {sample.filter(item => topic == "0" || topic === "" ? true : item.topic === topic)
+                        .filter(item => type == "0" || type === "" ? true : item.type === type)
+                        .map((item) =>
+                            <CustomCard id={item.id} topic={item.topic} type={item.type}/>
+                        )}
                 </Stack>
 
 
